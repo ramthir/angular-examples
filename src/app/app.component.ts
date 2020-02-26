e002ad9ab9a58be546b3155b98274e52a1c28e58
ramthir <ramesh.thiruchelvam@gmail.com>
2020-02-26 14:12:02 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { fromEvent, of } from "rxjs";
+import { skipUntil, takeUntil, repeat, withLatestFrom } from "rxjs/operators";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  name = "Angular";
+  down$ = fromEvent(document, "mousedown");
+  up$ = fromEvent(document, "mouseup");
+
+  constructor() {
+    this.up$.pipe(
+      withLatestFrom(this.down$))
+      .subscribe(console.log);
+  }
 }
